acfb42606fe2681959128162356927fe1704cec9
Ibr <ibrahmad18@gmail.com>
2024-09-15 09:51:13 +0100

    adding arrays

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    // primitive data types or scaller data types
+
+    // INTEGER
+    let x: i32 = -45;
+    let y: u64 = 245;
+
+    println!("Signed Integer {}", x);
+    print!("Unsigned Integer {} ", y);
+
+    let e: i32 = 214483647;
+    let i: i64 = 9223372036854775807;
+    
+    println!("Maximum value of i32: {}", e);
+    println!("Maximum value of i64: {}", i);
+
+    let is_snowing: bool = true;
+
+    println!("It is snowing ? {}", is_snowing);
+
+    let letter: char = 'a';
+    println!("Letters: {}", letter);
+
+    // Arrays 
+    let numbers: [i32; 8] = [1,2,3,4,5,6,7,8];
+
+    println!("Numer Array: {:?}", numbers);
+
+    let fruits: [&str; 3] = ["Apple", "Banana", "Mango"];
+
+    println!("Frutis Array {:?}", fruits );
+    
+
+
+    
+}
